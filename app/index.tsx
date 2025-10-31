2418cce6634927387f37acc96e911d623e3d7ee6
Anibal <anibalmoreno1998@gmail.com>
2025-10-31 17:24:52 +0000

    completado el searchbar

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,20 @@ import Greetings from "@/components/greetings";
 import PanelGrid from "@/components/panelGrid";
 import SearchBar from "@/components/searchBar";
 import styles from "@/theme";
-import { View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function Index() {
+
+      const [nombre, setNombre] = React.useState("");
+
   return (
     <View
       style={styles.mainContainer}>
       <Greetings name="Fernando" message="How can I help you today"/>
       <PanelGrid/>
-      <SearchBar/>
+      <SearchBar nombre={nombre} setNombre={setNombre}/>
+      <Text>{nombre}</Text>
     </View>
   );
 }
--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,15 +1,23 @@
 import styles from '@/theme';
 import { FontAwesome5 } from '@expo/vector-icons';
+import React from 'react';
 import { TextInput, View } from "react-native";
 
+interface SearchBarProps{
+    nombre: string;
+    setNombre: any;
+}
 
-export default function SearchBar (){
+
+export default function SearchBar ({nombre, setNombre}:SearchBarProps){
+
 
     return (
         <View style={styles.searchBarContainer}>
             <FontAwesome5 name="search" size={24} color="black" />
-            <TextInput
-                style={styles.searchBarInput}
+            <TextInput style={styles.searchBarInput}
+                    value={nombre}
+                    onChangeText={setNombre}
                 placeholder="Escribe aquí"
             />
             <FontAwesome5 name="question" size={24} color="black" />
